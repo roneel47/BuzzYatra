4445545a6bb2fc8090b61a89b2be3cde1a996f0e
roneelv <roneelv18@gmail.com>
2025-08-15 17:17:10 +0530

    Initialised the Db connection into the backend code

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,38 @@
-const express = require('express');
-const app = express();
+const express = require('express'); 
+const app = express(); 
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-//   res.send('Backend is running!');
-  res.send("<h1>Welcome to the Backend!</h1>");
+    res.send('Hello, World! This is the backend server running on port 5000.');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://roneelv:Yakbekithurgit@routename.enwbahs.mongodb.net/?retryWrites=true&w=majority&appName=routename";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
